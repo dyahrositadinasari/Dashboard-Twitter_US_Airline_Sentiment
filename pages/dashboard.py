6b69ca3ac21d81caa285d231b163bff61c300687
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-06-07 12:32:53 +0700

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -10,7 +10,7 @@ st.sidebar.title("Dashboard")
 
 url = "Dashboard-Twitter_US_Airline_Sentiment/Tweets.csv"
 
-st.chace(persist=True)
+st.cache(persist=True)
 def load_data():
   data = pd.read_csv(url)
   data['tweet_created'] = pd.to_datetime(data['tweet_created'])
