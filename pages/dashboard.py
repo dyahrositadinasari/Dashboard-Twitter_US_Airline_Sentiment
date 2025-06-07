9293a0fe67126aaa9a7ed59fe18d2eb26ada9349
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-06-07 12:32:01 +0700

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -5,3 +5,14 @@ import numpy as np
 
 st.title("Dashboard Twitter US Airline Sentiment")
 st.write("Sentiment Analysis of Tweets 🕊 about US Airline")
+
+st.sidebar.title("Dashboard")
+
+url = "Dashboard-Twitter_US_Airline_Sentiment/Tweets.csv"
+
+st.chace(persist=True)
+def load_data():
+  data = pd.read_csv(url)
+  data['tweet_created'] = pd.to_datetime(data['tweet_created'])
+  return data
+
