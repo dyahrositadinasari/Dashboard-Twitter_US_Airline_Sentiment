16ca57d20497a11df013c37b338b128b86128480
Dyah Dinasari <67815956+dyahrositadinasari@users.noreply.github.com>
2025-06-07 12:41:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.write("Welcome to Dashboard Twitter US Airline Sentiment")
+
+st.title("Dashboard Twitter US Airline Sentiment")
+st.write("Sentiment Analysis of Tweets 🕊 about US Airline")
+
+url = "Dashboard-Twitter_US_Airline_Sentiment/Tweets.csv"
+
+st.cache_data(persist=True)
+def load_data():
+  data = pd.read_csv(url)
+  data['tweet_created'] = pd.to_datetime(data['tweet_created'])
+  return data
+
+data = load_data()
+st.sidebar.subheader("Show random Tweet")
+random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative'))
